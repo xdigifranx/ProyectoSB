8c2d127f16de9ca85700f1581643c85a75ee503d
DigiFran <ifrancohernan@yahoo.com>
2025-05-14 20:47:36 -0300

    se crearon archivos y carpetas para el proyecto

---
+++ SiempreBonitasApp/src/component/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer >  
+            <p> 2023 Siempre Bonitas. All rights reserved.</p>
+            <p>Follow us on social media!</p>
+        </footer>
+    );
+}
+export default Footer;
+
---
+++ SiempreBonitasApp/src/component/Main/Main.tsx
@@ -0,0 +1,10 @@
+const Main = () => {
+
+    return (
+        <div>
+        <h1>Hola Mundo</h1>
+        <p>Bienvenidos a la aplicación de Siempre Bonitas</p>
+        </div>
+    );
+}
+export default Main;
